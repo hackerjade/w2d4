00ed2ed247aeb3c25a4909959ebc9424ef7da866
Jade McPherson <jmcpherson@Jades-MacBook-Air-2.local>
2015-04-30 10:46:38 -0700

    Created #find_promotion_row

--- piece.rb
+++ piece.rb
@@ -1,16 +1,24 @@
 class Piece
-  attr_reader :color
+  attr_reader :color, :promotion_row
   attr_accessor :pos, :promoted
 
   def initialize(color, pos, promoted = false)
     @color = color
     @pos = pos
     @promoted = promoted
+    @promotion_row = find_promotion_row
   end
 
-  def perform_slide
-    return true if is_valid?()
-    maybe_promote
+  def find_promotion_row
+    @pos[0] == 7 ? 0 : 7
+  end
+
+  def perform_slide(position)
+    delta = [position[0] - @pos[0], position[1] - @pos[1]]
+    return false if !move_diffs.include?(delta)
+    @pos = position
+    # maybe_promote
+    return true
   end
 
   def maybe_promote
@@ -44,6 +52,18 @@ class Piece
                     [1, 1]
                     ]
     end
-    #returns all directions a piece could move in
   end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  red = Piece.new(:red, [7,1])
+  black = Piece.new(:black, [0,1])
+  p [red.color, red.pos]
+  p [black.color, black.pos]
+  red.perform_slide([6,0])
+  black.perform_slide([1,0])
+  p [red.color, red.pos]
+  p [black.color, black.pos]
+  p red.promotion_row
+  p black.promotion_row
 end
