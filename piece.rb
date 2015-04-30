ba2e77c05d839d8e52ff3de2029579cb14975b70
Jade McPherson <jmcpherson@Jades-MacBook-Air-2.local>
2015-04-30 10:29:40 -0700

    Created move_diffs in pieces

--- piece.rb
+++ piece.rb
@@ -7,4 +7,43 @@ class Piece
     @pos = pos
     @promoted = promoted
   end
+
+  def perform_slide
+    return true if is_valid?()
+    maybe_promote
+  end
+
+  def maybe_promote
+    # @promoted == true if @pos[1] == #opposite side of board
+  end
+
+  def perform_jump
+    #remove the other piece we jumped over FROM BOARD
+  end
+
+  def is_valid?
+
+  end
+
+  def move_diffs
+    if color == :red && promoted == false
+      directions = [
+                    [-1, -1],
+                    [-1, 1]
+                    ]
+    elsif color == :black && promoted == false
+      directions = [
+                    [1, -1],
+                    [1, 1]
+                    ]
+    elsif promoted == true
+      directions = [
+                    [-1, -1],
+                    [-1, 1],
+                    [1, -1],
+                    [1, 1]
+                    ]
+    end
+    #returns all directions a piece could move in
+  end
 end
